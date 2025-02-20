b17ad1777f542a7c6a560c630a8b386bd54fca20
vladakoci <72820011+vladakoci@users.noreply.github.com>
2025-02-20 13:46:04 +0100

    Edited sandbox-aft account settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "sandbox-aft@brose.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "<SSO EMAIL>"
